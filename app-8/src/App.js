1e05c3eca5a8c99a3f6a994a2385ae0ce6400160
Christian Anderson <ca96187@gmail.com>
2019-04-17 07:34:00 -0600

    completed

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import axios from "axios"
+
 import "./App.css";
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      character: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://swapi.co/api/people/4")
+      .then(res => {
+        this.setState({
+          character: res.data
+        })
+      })
+      .catch(err => {
+        console.log(`GET err ${err}`)
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Star Wars Character</h1>
+        <br />
+        <h2>Name: {this.state.character.name}</h2>
+        <h2>Birth Year: {this.state.character.birth_year}</h2>
+        <h2>Height: {this.state.character.height} cm</h2>
+        <h2>Eye Color: {this.state.character.eye_color}</h2>
+        <br />
+        <h2>API: <a href="https://swapi.co">SWAPI</a></h2>
+
+
       </div>
     );
   }
